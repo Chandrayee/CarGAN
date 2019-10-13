4ee23e9fa7daf10423357496e604133741265ae4
Chandrayee Basu <basu.chandrayee@gmail.com>
2019-10-13 08:08:01 +0000

    added model evaluation and model download scripts

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,3 +1,3 @@
-wget -O models.zip 'https://www.dropbox.com/s/h8q5z4axfgzx9eb/models.zip?dl=0'
+wget -O models.zip 'https://www.dropbox.com/s/r40ob6epor4jlil/models.zip?dl=0'
 unzip -q models.zip
 rm -rf models.zip
